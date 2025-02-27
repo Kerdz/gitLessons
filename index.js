00be4499efd057ba42441ff34f230390a495955e
Kerdeam Findley <86058276+Kerdz@users.noreply.github.com>
2025-02-27 12:08:25 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById('game').addEventListener('click', function() {
+  alert('Game clicked');
+});
+
+document.getElementById('car').addEventListener('click', function() {
+  alert('Car clicked');
+});
